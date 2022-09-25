77a817da82140b03cdb4364a8dd507ad8c129537
nihaoma830 <155160615@qq.com>
2022-09-25 19:00:18 +0800

    回退测试

--- src/main/java/com/mytest/test.java
+++ src/main/java/com/mytest/test.java
@@ -8,6 +8,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("abs");
         System.out.println("sss");
+        System.out.println("fff");
+
 
 
     }
