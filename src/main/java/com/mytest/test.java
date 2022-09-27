5bc2c19875e8083de1ffc844dc3295cb557cbe05
nihaoma830 <155160615@qq.com>
2022-09-27 20:50:53 +0800

    cherry out

--- src/main/java/com/mytest/test.java
+++ src/main/java/com/mytest/test.java
@@ -8,8 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("abs");
         System.out.println("sss");
-        System.out.println("fff");
-
+        System.out.println("ccc");
 
 
     }
